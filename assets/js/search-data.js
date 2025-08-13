8a2717657566dc151688b345d5361adda9973071
Thrillcrazyer <53187684+Thrillcrazyer@users.noreply.github.com>
2025-08-13 06:36:22 +0000

    Deploying to gh-pages from @ pnubaelab/pnubaelab.github.io@8c9f4faedb92dc5615513a0402bde1f40859210f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/photos/";
           },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "nav-publications",
           title: "Publications",
           description: "Research publications and academic works.",
