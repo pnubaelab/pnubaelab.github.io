99027fe9428008ae0436092671738071a440dca7
Thrillcrazyer <53187684+Thrillcrazyer@users.noreply.github.com>
2026-02-17 01:23:46 +0000

    Deploying to gh-pages from @ pnubaelab/pnubaelab.github.io@4d2f8879e2165f4a8bf945cf63a490170527bd5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -228,6 +228,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/%5B2025%EB%85%84%EB%8F%84%20%EC%A0%9C4%ED%9A%8C%20%ED%95%B4%EC%96%91%EC%88%98%EC%82%B0%20%EA%B3%BC%ED%95%99%EA%B8%B0%EC%88%A0%20%ED%98%81%EC%8B%A0%ED%8F%AC%EB%9F%BC%5D%20%EB%B0%9C%ED%91%9C3%20-%20%EB%B6%80%EC%82%B0%EB%8C%80%ED%95%99%EA%B5%90%20%EB%B0%B0%ED%98%9C%EB%A6%BC%20%EA%B5%90%EC%88%98/";
+            },},{id: "news-부산대-영산대-북극항로-연구-시너지-공동-협력",
+          title: '부산대-영산대, 북극항로 연구 시너지 공동 협력',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2026-02-09-%EC%98%81%EC%82%B0%EB%8C%80%EB%B6%81%EA%B7%B9%ED%95%AD%EB%A1%9C/";
             },},{id: "people-nur-ahmad-wahid",
           title: 'Nur Ahmad Wahid',
           description: "M.S. Software Engineer at Diverta Inc.",
